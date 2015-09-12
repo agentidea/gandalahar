5c7e5c978fb47ed89fe691c3b7103f56fba54e4c
agentidea <GrantSteinfeld@gmail.com>
2015-09-12 17:33:00 -0400

    namespacing api

--- flaskRest.py
+++ flaskRest.py
@@ -10,40 +10,57 @@ from midsummer import getPlayers
 
 import sys
 import os.path
-here = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0,here)
 
-srcPth = os.path.dirname(__file__)+'/src'
+here = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, here)
+
+srcPth = os.path.dirname(__file__) + '/src'
 sys.path.append(srcPth)
 
 app = Flask(__name__)
 cors = CORS(app)
-api = Api(app, version='0.1.22', title='AgentIdea API', description='REST API to various backend analytics and stores')
+api = Api(app, version='0.1.23', title='AgentIdea API', description='REST API to various backend analytics and stores')
 
-@api.route('/midsummer')
+newsNS = api.namespace('news', 'news')
+humanNS = api.namespace('human', 'models')
+naturalLanguage = api.namespace('nlp', 'Natural Language Processing')
+allegroNS = api.namespace('allegro', 'triple store operations')
+
+@allegroNS.route('/triple')
+class Triple(Resource):
+    def get(self):
+        return {'s':'sub','p':'pred','o':'obj'}
+
+    def post(self):
+        return "save to triplestore"
+
+    def delete(self):
+        return "delete triple"
+
+
+
+@naturalLanguage.route('/midsummer')
 class Midsummer(Resource):
     def get(self):
-	ret = None 
-	try:
-            ret=getPlayers()
+        ret = None
+        try:
+            ret = getPlayers()
         except Exception as exp:
-            ret=exp.message
+            ret = exp.message
 
         return ret
 
-    def post(self):
-	api.abort(403)
 
-
-@api.route('/model/<attribute>')
+@humanNS.route('/model/<attribute>')
 class Model(Resource):
-    def get(self,attribute):
+    def get(self, attribute):
         return getAttribute(attribute)
 
     def post(self):
         api.abort(403)
 
-@api.route('/models')
+
+@humanNS.route('/models')
 class Models(Resource):
     def get(self):
         return getAttributes()
@@ -51,21 +68,24 @@ class Models(Resource):
     def post(self):
         api.abort(403)
 
-@api.route('/story/<o>')
+
+@newsNS.route('/story/<o>')
 class Story(Resource):
-    def get(self,o ):
+    def get(self, o):
         return getStory(o)
 
     def post(self):
         api.abort(403)
 
 
-@api.route('/breakingnews')
+@newsNS.route('/breakingnews')
 class BreakingNews(Resource):
     def get(self):
-	return getBreakingNews()
+        return getBreakingNews()
+
     def post(self):
-	api.abort(403)
+        api.abort(403)
+
 
 if __name__ == '__main__':
-    app.run(host='agentidea.com', port=7878)
+    app.run(host='localhost', port=7878)
